f75f2757a08f1504b48ac762625a087d1e3e0af8
ProPlays2312 <proplays2312@hotmail.com>
2024-10-03 09:56:20 +0530

    Fixing stuff

--- api/config.py
+++ api/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    DEBUG = os.environ.get('DEBUG')
+    DEBUG = False
     TESTING = False
 
 class DevelopmentConfig(Config):
@@ -21,5 +21,11 @@ config = {
     'default': DevelopmentConfig,
     "SECRET_KEY": os.environ.get('SECRET_KEY'),
     "DEBUG": os.environ.get('DEBUG'),
-    
+    "SESSION_TYPE": "filesystem",
+    "SESSION_PERMANENT": False,
+    "SESSION_EXPIRES": int(os.environ.get('SESSION_Expires')),
+    "SESSION_KEY_PREFIX": "session",
+    "SESSION_COOKIE_NAME": "token",
+    "SESSION_COOKIE_DOMAIN": "*",
+    "SESSION_COOKIE_SAMESITE": "Lax",
 }
